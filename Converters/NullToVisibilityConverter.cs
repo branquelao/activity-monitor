c3704034f179ed4e41935ac768355a309f9fc002
branquelao <henriquetozzonarciso@gmail.com>
2026-02-03 09:16:17 -0300

    Now applications logo's that couldn't be loaded return as null app symbol

---
+++ Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace ActivityMonitor.Converters
+{
+    // Shows element when value is NOT null
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Shows element when value IS null
+    public class InverseNullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
